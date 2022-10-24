65bbbc26f54222627f383c7f771b70d16091dc19
Sharmin Moon <sharminmoon0308@gmail.com>
2022-10-24 22:25:02 +0600

    course category api added

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const port = process.env.PORT || 5005;
 
 app.use(cors());
 
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("course API Running");
+});
+
+app.get("/course-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category-wise-course/:id", (req, res) => {
+  const id = req.params.id;
+
+  const category_course = courses.filter((course) => course.category_id === id);
+  res.send(category_course);
 });
 
 app.listen(port, () => {
